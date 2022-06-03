4610fffb7bf849a1d7ac5c63dd947ec9ed220961
polatburak <burak.polatt52@gmail.com>
2022-06-03 18:12:23 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import javax.swing.*;
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                openingPage oP = new openingPage();
+                oP.setVisible(true);
+            }
+        });
+
     }
 
 }
--- src/openingPage.java
+++ src/openingPage.java
@@ -1,2 +1,31 @@
-public class openingPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class openingPage extends JFrame {
+    private JTextField benutzernameField1;
+    private JPasswordField passwordField1;
+    private JButton logInButton;
+    private JPanel panel1;
+
+    public openingPage(){
+        add(panel1);
+        setSize(400,200);
+        setTitle("Log-in");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String benutzername;
+                String passwort;
+
+                benutzername = benutzernameField1.getText();
+                passwort = String.valueOf(passwordField1.getPassword());
+
+                System.out.println(benutzername+":"+passwort);
+            }
+        });
+    }
 }
